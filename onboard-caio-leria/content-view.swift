031af53b8c6e84ca1bd36477f1290596c77b884a
Caio <caioleria2100@gmail.com>
2026-01-09 15:56:53 -0300

    login page

--- onboard-caio-leria/content-view.swift
+++ onboard-caio-leria/content-view.swift
@@ -1,8 +1,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text("Hello, World!")
+        Text("Bem vindo(a) à Taqtile!")
+            .font(.largeTitle).fontWeight(.bold)
+        VStack {
+            Text("Login")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title2)
+           TextField(
+            "Login",
+            text: $username
+           )
+           .border(Color.gray)
+            
+        }
+        .padding()
+        VStack {
+            Text("Password")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title2)
+            TextField(
+                "Password",
+                text: $password
+            )
+            .border(Color.gray)
+        }
+        .padding()
+        VStack {
+            Button("Login") {
+            }
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(Color.white)
+            .background(Color.blue)
+            .padding(.horizontal)
+        }
     }
 }
 
