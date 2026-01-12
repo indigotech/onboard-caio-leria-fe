13539d09dc42fbaebc3324199abd7c880ed4c4f8
Caio <caioleria2100@gmail.com>
2026-01-12 12:07:30 -0300

    Separando login na pasta login e criando lógica de validação de email e senha

--- onboard-caio-leria/LoginPage/LoginPage.swift
+++ onboard-caio-leria/LoginPage/LoginPage.swift
@@ -2,62 +2,60 @@ import SwiftUI
 
 struct LoginView: View {
     @StateObject private var viewModel: LoginViewModel = .init()
-    
     var body: some View {
-        Text("Bem vindo(a) à Taqtile!")
-            .font(.largeTitle).fontWeight(.bold)
-        
-        VStack {
-            Text("Login")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.title2)
-            TextField(
-                "seuemail@email.com",
-                text: $viewModel.email
-            )
-            .border(Color.gray)
-            .foregroundStyle(.black)
-        }
-        .padding(.horizontal)
-        .padding(.bottom, 2)
-        
-        VStack {
-            Text("Senha")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.title2)
-            SecureField(
-                "Senha",
-                text: $viewModel.password
-            )
-            .border(Color.gray)
-        }
-        .padding(.horizontal)
-        .padding(.bottom, 2)
-        
-        VStack {
-            if viewModel.isLoading {
-                ProgressView("Carregando")
-                    .progressViewStyle(CircularProgressViewStyle())
-            } else {
-                Button("Login") {
-                    viewModel.validatingCredentials()
-                }
-                .frame(maxWidth: .infinity)
-                .foregroundStyle(Color.white)
-                .background(Color.blue)
-                .padding(.horizontal)
+        NavigationStack {
+            Text("Bem vindo(a) à Taqtile!")
+                .font(.largeTitle).fontWeight(.bold)
+            VStack {
+                Text("Login")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.title2)
+                TextField(
+                    "seuemail@email.com",
+                    text: $viewModel.email
+                )
+                .border(Color.gray)
+                .foregroundStyle(.black)
             }
+            .padding(.horizontal)
+            .padding(.bottom, 2)
             
-        }
-        
-        if !viewModel.validationErrorText.isEmpty {
-            Text(viewModel.validationErrorText)
-                .foregroundColor(.red)
-        }
-        
-        if !viewModel.textError.isEmpty {
-            Text(viewModel.textError)
-                .foregroundColor(.red)
+            VStack {
+                Text("Senha")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.title2)
+                SecureField(
+                    "Senha",
+                    text: $viewModel.password
+                )
+                .border(Color.gray)
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 2)
+            
+            VStack {
+                if viewModel.isLoading {
+                    ProgressView("Carregando")
+                        .progressViewStyle(CircularProgressViewStyle())
+                } else {
+                    Button("Login") {
+                        viewModel.validatingCredentials()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(Color.white)
+                    .background(Color.blue)
+                    .padding(.horizontal)
+                }
+                
+            }.navigationDestination(isPresented: $viewModel.isLoggedIn) {
+                UsersView()}
+            
+            if !viewModel.validationErrorText.isEmpty {
+                Text(viewModel.validationErrorText)
+            }
+            if !viewModel.textError.isEmpty {
+                Text(viewModel.textError)
+            }
         }
     }
 }
