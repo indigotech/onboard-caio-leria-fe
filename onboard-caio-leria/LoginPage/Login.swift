cb160c1eee4b7340ed210053d3a3ae3f80d337fc
Caio <caioleria2100@gmail.com>
2026-01-13 13:20:40 -0300

    correcting error message

--- onboard-caio-leria/LoginPage/Login.swift
+++ onboard-caio-leria/LoginPage/Login.swift
@@ -2,6 +2,7 @@ class Login: Codable{
     var email: String = ""
     var password: String = ""
     var token: String = ""
+    var errorMessage: String = ""
     
     struct LoginResponse: Codable {
         let data: TokenContainer
@@ -9,5 +10,14 @@ class Login: Codable{
 
     struct TokenContainer: Codable {
         let token: String
+    }
+    
+    struct LoginError: Codable {
+        let errors: [ErrorResponse]?
+    }
+    struct ErrorResponse: Codable {
+        let name: String
+        let code: Int
+        let message: String
     }
 }
